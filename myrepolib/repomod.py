4e5cde6eacd1f12ceb67790900699df0d21c254f
Daniel Smith <dan@Daniels-MacBook-Pro.local>
2020-09-22 13:31:53 -0400

    try except

--- myrepolib/repomod.py
+++ myrepolib/repomod.py
@@ -7,23 +7,28 @@ def myfunc():
 
 app = Flask(__name__)
 
-client = bigquery.Client()
+try:
+    client = bigquery.Client()
 
-# Perform a query.
-QUERY = (
-    'SELECT * FROM `myrepo-290018.434project.covid`'
-    'LIMIT 10')
-query_job = client.query(QUERY)  # API request
-rows = query_job.result()  # Waits for query to finish
+    # Perform a query.
+    QUERY = (
+        'SELECT * FROM `myrepo-290018.434project.covid`'
+        'LIMIT 10')
+    query_job = client.query(QUERY)  # API request
+    rows = query_job.result()  # Waits for query to finish
 
-#for row in rows:
-    #print(row.name)
+    #for row in rows:
+        #print(row.name)
 
-@app.route('/')
-def hello():
-    for row in rows:
-        print(row.name)
-    return jsonify(value = "Hello 434 class!", test=row.name)
+    @app.route('/')
+    def hello():
+        for row in rows:
+            print(row.name)
+        return jsonify(value = "Hello 434 class!", test=row.name)
+except:
+    @app.route('/')
+    def hello():
+        return jsonify(value = "Hello 434 class!")
 
 if __name__ == '__main__':
     app.run()
