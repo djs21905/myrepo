f4feafe039aa4d94fde15067ae3ef610db2ca703
Daniel Smith <dan@Daniels-MacBook-Pro.local>
2020-09-22 13:20:49 -0400

    testing bigquery

--- myrepolib/repomod.py
+++ myrepolib/repomod.py
@@ -1,17 +1,29 @@
+from flask import Flask
+from flask import jsonify
+from google.cloud import bigquery
+
 def myfunc():
     return 1
 
-#Testing a pass
-
-
-from flask import Flask
-from flask import jsonify
 app = Flask(__name__)
 
+client = bigquery.Client()
+
+# Perform a query.
+QUERY = (
+    'SELECT * FROM `myrepo-290018.434project.covid`'
+    'LIMIT 10')
+query_job = client.query(QUERY)  # API request
+rows = query_job.result()  # Waits for query to finish
+
+#for row in rows:
+    #print(row.name)
 
 @app.route('/')
 def hello():
-    return jsonify(value = "Hello 434 class!")
+    for row in rows:
+        print(row.name)
+    return jsonify(value = "Hello 434 class!", test=row.name)
 
 if __name__ == '__main__':
     app.run()
