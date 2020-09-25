e9d847a7dda9a9de02a811d6751e3ad2959dc08f
Daniel Smith <dan@Daniels-MacBook-Pro.local>
2020-09-25 11:29:37 -0400

    Uploading HTML templates and working on form sub

--- myrepolib/repomod.py
+++ myrepolib/repomod.py
@@ -1,8 +1,7 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, render_template, request, redirect
 from google.cloud import bigquery
 #import os 
-
+#from flask import jsonify
 
 
 
@@ -11,7 +10,7 @@ def myfunc():
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def hello():
     #abspath = os.path.abspath(__file__)
     #dname = os.path.dirname(abspath)
@@ -24,7 +23,19 @@ def hello():
     result = query_job.result() 
     for item in result:
         a = item[0] 
-    print(a,result)
-    return jsonify(value = "Hello 434 class!", test= a)
+    return render_template('hello.html', a = a)
+
+@app.route('/test', methods=["GET", "POST"])
+def test():
+    if request.method == 'POST':
+        param1 =  request.form['Param1']
+        param2 =  request.form['Param2']
+        param3 =  request.form['Param3']
+        param4 =  request.form['Param4']
+        result= [param1,param2,param3,param4]
+
+        #Send this data to GCP AutoML and have it return the prediction
+    return render_template('test.html', result=result)
+    
 if __name__ == '__main__':
     app.run()
